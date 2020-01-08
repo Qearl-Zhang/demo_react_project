22a9ab6048f6e5cca20b7250ad31c9b351ab0c15
caotyf <caotyf@yonyou.com>
2020-01-08 16:01:31 +0800

    '新建redux分支'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from "react";
-import { Route, Switch,Redirect } from "react-router-dom";
-
-import Admin from "./pages/admin/admin";
-import Login from "./pages/login/login";
 
 export default class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/admin" component={Admin} />
-        <Redirect to="/login"/>
-      </Switch>
-    );
+      <div>App</div>
+    )
   }
 }
