c859ada46fa79824a59b1041ca2e5c1e6eafd82e
caotyf <caotyf@yonyou.com>
2020-01-09 15:12:28 +0800

    更新

---
+++ 05_src_redux-dev-tools/redux/action_creators.js
@@ -0,0 +1,12 @@
+import {INCREMENT,DECREMENT} from './action_types'
+// 专门用于制造action对象
+export const increment =value=>{return {type:INCREMENT,data:value}} //分发同步action
+export const decrement = value=>({type:DECREMENT,data:value})
+//分发同步action
+export const incrementAsync = (value,timeout)=>{  //分发异步action
+  return (dispatch)=>{
+    setTimeout(() => {
+      dispatch(increment(value))  //分发同步action
+    }, timeout);
+  }
+}
---
+++ 05_src_redux-dev-tools/redux/action_types.js
@@ -0,0 +1,3 @@
+// 把所有用到的方法名,全部定义成常量,防止后续因名称拼错导致出现错误
+export const INCREMENT = 'incerment'
+export const DECREMENT = 'decerment'
