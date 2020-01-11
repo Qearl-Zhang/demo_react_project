9b74401bc48d4159dcccf27c110becc5dd749398
caotyf <caotyf@yonyou.com>
2020-01-11 17:23:02 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Route, Switch,Redirect } from "react-router-dom";
 
-import Admin from "./pages/admin/admin";
-import Login from "./pages/login/login";
+import Admin from "./container/admin/admin";
+import Login from "./container/login/login";
 
 export default class App extends Component {
   render() {
